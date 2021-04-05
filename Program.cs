31bdc60f5365b044ce7d8fbc5bd2066adb0c8559
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-04-05 13:12:18 -0400

    minor updates

--- Program.cs
+++ Program.cs
@@ -7,22 +7,22 @@ namespace Measurement_Application_github_practice
         static void Main(string[] args)
         {
             // User inputs length and width, program calculates area and peremiter
-
-
-
-
-
-
             Console.WriteLine("Length of rectangle?");
             string Length = Console.ReadLine();
+
             double LengthF = double.Parse(Length);
             Console.WriteLine("Width of rectangle?");
+
             string Width = Console.ReadLine();
             double WidthF = double.Parse(Width);
+
             double area = LengthF * WidthF;
             double peremiter = WidthF * 2 + LengthF * 2;
-            Console.WriteLine("Area is " + area );
-            Console.WriteLine("Peremiter is " + peremiter);
+
+            Console.WriteLine($"Area is  { area }");
+            Console.WriteLine($"Peremiter is  { peremiter }");
+
+            Console.ReadLine();
         }
     }
 }
